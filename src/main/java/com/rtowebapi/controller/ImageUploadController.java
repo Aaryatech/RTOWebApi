0d824d2702b6d522cdf9499d3dc9339b8d654ed7
lenovo <lenovo@lenovo-Lenovo-ideapad-320-15IKB>
2018-11-02 14:59:12 +0530

    Image Upload..Sac

---
+++ src/main/java/com/rtowebapi/controller/ImageUploadController.java
@@ -0,0 +1,102 @@
+package com.rtowebapi.controller;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+
+import javassist.bytecode.stackmap.BasicBlock.Catch;
+
+@RestController
+public class ImageUploadController {
+
+	//private static String DOC_URL = "/opt/tomcat-latest/webapps/uploads/SUGGESTION/";
+	
+	private static String DOC_URL="/home/lenovo/Documents/RtoUpload/";
+	
+	
+	//private static String BILL_FOLDER ="/home/maxadmin/Desktop/photos/";
+	
+	@RequestMapping(value = { "/photoUpload" }, method = RequestMethod.POST)
+	public @ResponseBody Info getFarmerContract(@RequestParam("file") MultipartFile[] uploadfile , @RequestParam("imageName") List<String> imageName,@RequestParam("type") String type) {
+System.err.println(" no  of files to push " +uploadfile.length);
+		Info info = new Info();
+
+		//System.out.println("File Name " + imageName.toString());
+
+		try {
+
+			saveUploadedFiles(uploadfile ,  imageName,type);
+
+			info.setError(false);
+			info.setMessage("File uploaded successfully");
+
+		} catch (IOException e) {
+
+			e.printStackTrace();
+			info.setError(true);
+			info.setMessage("File upload failed");
+		}
+
+		return info;
+	}
+
+	// save file
+	private void saveUploadedFiles(MultipartFile[] files,  List<String> imageName,String type) throws IOException {
+
+		/*try {
+		for (MultipartFile file : files) {
+			Path path=null;
+			if (file.isEmpty()) {
+				continue; 
+			}
+            if(type.equalsIgnoreCase("1"))
+            {
+			 path =Paths.get(DOC_URL + imageName);
+            }
+
+            
+			byte[] bytes = file.getBytes();
+			
+			Files.write(path, bytes);
+
+		}
+
+	
+	}*/
+		
+		try {
+			for (int i=0;i<files.length;i++) {
+				Path path=null;
+				
+	            if(type.equalsIgnoreCase("1"))
+	            {
+				 path =Paths.get(DOC_URL + imageName.get(i));
+	            }
+
+	            
+				byte[] bytes = files[i].getBytes();
+				
+				Files.write(path, bytes);
+
+			}
+
+		
+		}
+	catch(Exception e)
+	{
+		e.printStackTrace();
+	}
+
+}
+}
