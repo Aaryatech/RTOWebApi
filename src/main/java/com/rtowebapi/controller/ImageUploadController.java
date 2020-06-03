afbf9e98530dca208c443725939114e0cec89bdb
maddy <maddy@maddy-Inspiron-3541>
2020-06-03 16:18:05 +0530

    //maddy

--- src/main/java/com/rtowebapi/controller/ImageUploadController.java
+++ src/main/java/com/rtowebapi/controller/ImageUploadController.java
@@ -4,40 +4,32 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Arrays;
 import java.util.List;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-
-import javassist.bytecode.stackmap.BasicBlock.Catch;
-
 @RestController
 public class ImageUploadController {
 
-	//private static String DOC_URL = "/opt/tomcat-latest/webapps/uploads/SUGGESTION/";
+	private static String DOC_URL="C:/Users/MAXADMIN/Desktop/SecurityApp Files";
 	
-	//private static String DOC_URL="/home/lenovo/Documents/RtoUpload/";
-	//private static String DOC_URL=	"/home/aaryate1/tomcat.aaryatechindia.in/tomcat-8.0.18/webapps/rtodocupload/";
-	private static String DOC_URL=	"/home/tomcataaryatechi/tomcat.aaryatechindia.in/tomcat-8.0.18/webapps/rtodocupload/";
-	
-	//private static String BILL_FOLDER ="/home/maxadmin/Desktop/photos/";
-	
-	@RequestMapping(value = { "/photoUpload" }, method = RequestMethod.POST)
-	public @ResponseBody Info getFarmerContract(@RequestParam("file") MultipartFile[] uploadfile , @RequestParam("imageName") List<String> imageName,@RequestParam("type") String type) {
-System.err.println(" no  of files to push " +uploadfile.length);
-		Info info = new Info();
+	//private static String DOC_URL = "/home/tomcataaryatechi/tomcat.aaryatechindia.in/tomcat-8.0.18/webapps/rtodocupload/";
 
-		//System.out.println("File Name " + imageName.toString());
+	@PostMapping("/photoUpload")
+	public @ResponseBody Info getFarmerContract(@RequestParam("file") MultipartFile[] uploadfile,
+			@RequestParam("imageName") List<String> imageName, @RequestParam("type") String type) {
+		
+		System.err.println(" no  of files to push " + uploadfile.length);
+		
+		Info info = new Info();
 
 		try {
 
-			saveUploadedFiles(uploadfile ,  imageName,type);
+			saveUploadedFiles(uploadfile, imageName, type);
 
 			info.setError(false);
 			info.setMessage("File uploaded successfully");
@@ -53,54 +45,45 @@ System.err.println(" no  of files to push " +uploadfile.length);
 	}
 
 	// save file
-	private void saveUploadedFiles(MultipartFile[] files,  List<String> imageName,String type) throws IOException {
+	private void saveUploadedFiles(MultipartFile[] files, List<String> imageName, String type) throws IOException {
 
-		/*try {
-		for (MultipartFile file : files) {
-			Path path=null;
-			if (file.isEmpty()) {
-				continue; 
-			}
-            if(type.equalsIgnoreCase("1"))
-            {
-			 path =Paths.get(DOC_URL + imageName);
-            }
-
-            
-			byte[] bytes = file.getBytes();
-			
-			Files.write(path, bytes);
-
-		}
-
-	
-	}*/
 		
+		/*
+		 * try { for (MultipartFile file : files) { Path path=null; if (file.isEmpty())
+		 * { continue; } if(type.equalsIgnoreCase("1")) { path =Paths.get(DOC_URL +
+		 * imageName); }
+		 * 
+		 * 
+		 * byte[] bytes = file.getBytes();
+		 * 
+		 * Files.write(path, bytes);
+		 * 
+		 * }
+		 * 
+		 * 
+		 * }
+		 */
+ 
 		try {
-			for (int i=0;i<files.length;i++) {
-				Path path=null;
-				
-	            if(type.equalsIgnoreCase("1"))
-	            {
-	            	
-	            String name=imageName.get(i).substring(1, imageName.get(i).length()-1);
-	            
-				 path =Paths.get(DOC_URL + name);
-	            }
+			for (int i = 0; i < files.length; i++) {
+				Path path = null;
 
-	            
+				if (type.equalsIgnoreCase("1")) {
+
+					String name = imageName.get(i).substring(1, imageName.get(i).length() - 1);
+
+					path = Paths.get(DOC_URL + name);
+				}
+
 				byte[] bytes = files[i].getBytes();
-				
+
 				Files.write(path, bytes);
 
 			}
 
-		
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-	catch(Exception e)
-	{
-		e.printStackTrace();
-	}
 
-}
+	}
 }
